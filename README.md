9872ec65c950b51634c6aa77f8ebacac65b1fb62
Florent Kosmala <kosflorent@gmail.com>
2023-07-08 10:15:45 +0200

    Initial release

--- README.md
+++ README.md
@@ -1,2 +1,48 @@
-# tokendistrib
-Automatic HiveEngine token distributor
+# TokenDistrib
+
+Token Distrib is a HIVE blockchain tool to automatize token distribution for community. This repo is the source code for the official install of [TokenDistrib](https://distrib.hivelive.me/).
+
+## Installation
+
+To install your instance of **Token Distrib**, you must have a Web server with PHP 8. This project work with [HiveSQL](https://hivesql.io/) and need the installation of [MS SQL drivers](https://learn.microsoft.com/en-us/sql/connect/odbc/download-odbc-driver-for-sql-server?view=sql-server-ver16).
+
+---
+
+#### Example: Install for Debian 13 Trixie
+
+Install your web server and add PHP and dependencies :
+```
+apt install git unixodbc-dev php8.2 php8.2-dev php8.2-xml php8.2-intl composer
+pecl install sqlsrv pdo_sqlsrv
+```
+
+After that just add the `extension=pdo_sqlsrv.so` line in your php.ini file. Restart your web server and it's OK :)
+
+---
+
+### Configure your web server
+
+TokenDistrib is based on Slim 4 Framework. Depending on the webserver you use, just check the [Slim 4 Web servers documentation](https://www.slimframework.com/docs/v4/start/web-servers.html) to find the config you need.
+
+### Clone this repo
+
+Just go to the folder you want to install **TokenDistrib** and clone this repo :
+
+```bash
+git clone https://github.com/fkosmala/tokendistrib
+```
+
+### Install TokenDistrib deps
+
+TokenDistrib use [Composer](https://getcomposer.org/) to manage dependencies. Just go to the project folder and made a composer install :
+
+```bash
+cd tokendistrib
+composer install
+```
+
+## Fill Config file
+
+go to the `config/` folder and copy the `db.sample.json` to `db.json`. After that, edit the file to add your HiveSQL credentials.
+
+That's all folks ! Open your browser and start to use your **TokenDistrib** Instance !
---
+++ public/index.php
@@ -0,0 +1,138 @@
+<?php
+
+use DI\Container;
+use Hive\PhpLib\Hive\Condenser as HiveCondenser;
+use Hive\PhpLib\HiveEngine\Account as HeAccount;
+use Noodlehaus\Config;
+use Slim\Factory\AppFactory;
+use Slim\Views\Twig;
+use Slim\Views\TwigMiddleware;
+
+require __DIR__ . '/../vendor/autoload.php';
+
+/* Define the config array in const */
+const APICONFIG = [
+    "debug" => false,
+    "disableSsl" => false,
+    "heNode" => "api.hive-engine.com/rpc",
+    "hiveNode" => "anyx.io",
+    "throwExceptions" => false
+];
+
+// Create Container
+$container = new Container();
+AppFactory::setContainer($container);
+
+// If DB config file not exist, create it from sample file
+$confDir =  __DIR__ . '/../config/';
+if ((file_exists($confDir . 'db.sample.json')) && (!file_exists($confDir . 'db.json'))) {
+    copy($confDir . 'db.sample.json', $confDir . 'db.json');
+}
+
+// Set DB config in container
+$container->set('db', function () {
+    $conf = new Config(__DIR__ . '/../config/db.json');
+    return $conf;
+});
+
+// Set Twig template engine for view in Container
+$container->set('view', function () {
+    return Twig::create(
+        __DIR__ . '/../resources/views',
+        ['cache' => false]
+    );
+});
+
+// Create App
+$app = AppFactory::create();
+
+// Add Twig-View Middleware
+$app->add(TwigMiddleware::createFromContainer($app));
+
+/**
+ * Index: This route is to display the index page.
+ */
+$app->get('/', function ($request, $response) {
+    return $this->get('view')->render($response, 'index.html');
+})->setName('index');
+
+/**
+ * Account: This route get delegatees from the specified account.
+ */
+$app->get('/account/{name}', function ($request, $response, $args) {
+    $account = $args['name'];
+
+    /* Get Delegatees with HiveSQL query */
+    $config = $this->get('db');
+    $connectInfo = [
+        "Database" => $config->get('dbName'),
+        "UID" => $config->get('dbUser'),
+        "PWD" => $config->get('dbPasswd'),
+        "Encrypt" => true,
+        "TrustServerCertificate" => true
+    ];
+
+    $conn = sqlsrv_connect($config->get('dbServer'), $connectInfo);
+
+    if (!$conn) {
+        $payload = "Connection error";
+        die(print_r(sqlsrv_errors(), true));
+    } else {
+        $query = "SELECT delegator, vests FROM Delegations WHERE delegatee = ? ORDER BY vests DESC";
+        $statement = sqlsrv_query($conn, $query, array($account));
+    }
+
+    if (!$statement) {
+        $payload = "Query error";
+        die(print_r(sqlsrv_errors(), true));
+    } else {
+        $arr = [];
+        while ($row = sqlsrv_fetch_array($statement, SQLSRV_FETCH_ASSOC)) {
+            $arr[] = [
+                "account" => $row['delegator'],
+                "vests" => $row['vests']
+            ];
+        }
+    }
+    
+    $api = new HiveCondenser(APICONFIG);
+    $globalProps = json_encode($api->getDynamicGlobalProperties(), JSON_PRETTY_PRINT);
+    
+    /* Convert VESTS to HP for each account and create an array with data */
+    $bcVars = json_decode($globalProps, true);
+    $vests = [];
+    $vests['tvfh'] = (float) $bcVars['total_vesting_fund_hive'];
+    $vests['tvs'] = (float) $bcVars['total_vesting_shares'];
+    $totalVests = $vests['tvfh'] / $vests['tvs'];
+    $accData = [];
+    foreach ($arr as $acc) {
+        $hp = round((float) $acc['vests'] * $totalVests, 3);
+        $accData[] = [
+            "account" => $acc['account'],
+            "hp" => $hp
+        ];
+    }
+
+    /* Convert data array in JSON format and send to response */
+    $payload = json_encode($accData, JSON_PRETTY_PRINT);
+    $response->getBody()->write($payload);
+
+    return $response->withHeader('Content-Type', 'application/json');
+})->setName('account');
+
+/**
+ * Tokens: This route get all available Hive-Engine tokens from specified sender account.
+ */
+$app->get('/tokens/{name}', function ($request, $response, $args) {
+    $account = $args['name'];
+
+    $heApi = new HeAccount(APICONFIG);
+    $payload = json_encode($heApi->getAccountBalance($account), JSON_PRETTY_PRINT);
+
+    $response->getBody()->write($payload);
+
+    return $response->withHeader('Content-Type', 'application/json');
+})->setName('tokens');
+
+// Run app
+$app->run();
